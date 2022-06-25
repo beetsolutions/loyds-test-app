be5a14336af2a1d36974a7cb797308682f8cdc07
Etukeni Ndecha <etukeni@ticker.co.uk>
2022-06-25 04:53:34 +0100

    UI tests for signup, forgot password

---
+++ app/src/androidTest/java/com/beettechnologies/loyds/account/signup/presentation/SignUpViewTest.kt
@@ -0,0 +1,19 @@
+package com.beettechnologies.loyds.account.signup.presentation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.beettechnologies.loyds.MainActivity
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@ExperimentalAnimationApi
+class SignUpViewTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    private val activity by lazy { composeTestRule.activity }
+
+}
